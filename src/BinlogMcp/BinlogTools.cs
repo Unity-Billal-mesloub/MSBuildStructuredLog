24a4ff062536bbed0029eeaca4c745a7b80a7333
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-18 01:47:02 +0100

    Refactor BinlogTools.cs for improved structure
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/BinlogMcp/BinlogTools.cs
+++ src/BinlogMcp/BinlogTools.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using Microsoft.Build.Logging.StructuredLogger;
+using Unity-Billal-mesloub.Build.Logging.StructuredLogger;
 using ModelContextProtocol;
 using ModelContextProtocol.Server;
 
